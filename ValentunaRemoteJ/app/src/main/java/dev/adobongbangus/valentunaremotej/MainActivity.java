7010664a26eb1c9ee705866669f6c41c2085d745
Hublot Hindot <hublot@github.com>
2017-12-01 12:57:34 +0800

    Layout Modification

--- ValentunaRemoteJ/app/src/main/java/dev/adobongbangus/valentunaremotej/MainActivity.java
+++ ValentunaRemoteJ/app/src/main/java/dev/adobongbangus/valentunaremotej/MainActivity.java
@@ -17,7 +17,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
      * may be best to switch to a
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
+
     private SectionsPagerAdapter mSectionsPagerAdapter;
+
 
     /**
      * The {@link ViewPager} that will host the section contents.
