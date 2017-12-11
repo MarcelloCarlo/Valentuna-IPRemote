569279fb8bb4649983538f1f0925383d8d400a00
Hublot Hindot <hindot@github.com>
2017-12-12 02:20:15 +0800

    Minor Layout Changes

--- ValentunaRemoteJ/app/src/main/java/dev/adobongbangus/valentunaremotej/WelcomePage.java
+++ ValentunaRemoteJ/app/src/main/java/dev/adobongbangus/valentunaremotej/WelcomePage.java
@@ -8,13 +8,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 
 public class WelcomePage extends Fragment {
 
+    Button autoDiscoverbtn,manualInputbtn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View welcomeView = inflater.inflate(R.layout.welcome_tab, container, false);
+        autoDiscoverbtn = welcomeView.findViewById(R.id.btnAutoDiscoverDevice);
+        manualInputbtn = welcomeView.findViewById(R.id.btnAddDeviceManual);
+
+      autoDiscoverbtn.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              //alert dialog for searching devices
+
+          }
+      });
+
+      manualInputbtn.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              //manual input
+          }
+      });
+
         return welcomeView;
     }
 }
